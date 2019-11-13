023633cc34d69e6d4ca4ccf74e8b06d921eba2d5
원보연 <qhdus114@gmail.com>
2019-11-13 20:10:06 +0900

    register

--- Team2_EmpManager/src/kr/or/bit/service/RegisterOkService_won.java
+++ Team2_EmpManager/src/kr/or/bit/service/RegisterOkService_won.java
@@ -47,7 +47,7 @@ public class RegisterOkService_won implements Action {
 			forward = new ActionForward();
 			if (result > 0) {
 				forward.setRedirect(false); // forward 방식
-				forward.setPath("//WEB-INF/views/register/register.jsp");
+				forward.setPath("/WEB-INF/views/login/Login.jsp");
 			} else { // -1 (제약, 컬럼길이 문제)
 				forward.setRedirect(false); // forward 방식
 				forward.setPath("/WEB-INF/views/register/register.jsp");
