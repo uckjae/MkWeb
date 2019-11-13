8b803384cb46f5b5022d08fbd0ab3f9a16b4f198
dajeong.yoon <djyoon956@gmail.com>
2019-11-13 17:07:40 +0900

    registerOk 서비스로 이름 변경

--- Team2_EmpManager/src/kr/or/bit/service/RegisterOkService.java
+++ Team2_EmpManager/src/kr/or/bit/service/RegisterOkService.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import kr.or.bit.action.Action;
 import kr.or.bit.action.ActionForward;
 
-public class RegisterService implements Action {
+public class RegisterOkService implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
